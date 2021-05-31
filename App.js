f42515ad5e68ce5eb993a17627f2c1ef83a5dc34
Robin Rodgers <cr43to@insim.biz>
2021-05-31 14:11:08 +0200

    first setup env files and server error handling

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { KeyboardAvoidingView, StyleSheet } from 'react-native';
 import './src/common/Config';
 import './src/common/api/Api';
 import './src/common/api/Interceptors';
+import { IS_IOS} from './src/common/Constants';
 import Router from './src/Router';
 import AuthContextProvider from './src/authentication/store/AuthContext';
+import { colors } from './src/common/Colors';
 
 const App: () => Node = () => {
 
   return (
-    <AuthContextProvider>
-      <Router />
-    </AuthContextProvider>
+      <AuthContextProvider>
+        <Router />
+      </AuthContextProvider>
   );
 };
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
